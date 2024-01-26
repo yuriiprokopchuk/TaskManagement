89480aa83c9f3d729b44d2484028e9463058e138
Прокопчук Юрий <yurii.prokopchuk@emotorwerks.com>
2024-01-26 21:10:22 +0200

    fix

--- TaskManagement.Bus/obj/Debug/net6.0/TaskManagement.Bus.Infrastructure.AssemblyInfo.cs
+++ TaskManagement.Bus/obj/Debug/net6.0/TaskManagement.Bus.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagement.Bus.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c22cd274471172d710fc28ddfbd082859dd000e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagement.Bus.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagement.Bus.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TaskManagement.Services.Tests/obj/Debug/net6.0/TaskManagement.Services.Tests.AssemblyInfo.cs
+++ TaskManagement.Services.Tests/obj/Debug/net6.0/TaskManagement.Services.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagement.Services.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c22cd274471172d710fc28ddfbd082859dd000e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagement.Services.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagement.Services.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
